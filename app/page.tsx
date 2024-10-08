12f503f73fc1ce49525ad5c96b24d7f72f011b55
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-10-08 22:17:49 +0200

    Create HowItWorksSection.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/app/components/landing-page/HeroSection";
 import FunctionsSection from "@/app/components/landing-page/FunctionsSection";
+import HowItWorksSection from "@/app/components/landing-page/HowItWorksSection";
 
 export default function Home() {
   return (
     <div className="max-w-[1440px] px-6 mx-auto">
       <HeroSection />
       <FunctionsSection />
+      <HowItWorksSection />
     </div>
   );
 }
